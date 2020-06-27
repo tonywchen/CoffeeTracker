465614b61464857b03a70e6f491b057a48f20bb2
Tony Wei-Han Chen <tonywh.chen@gmail.com>
2020-06-27 12:03:09 -0400

    cleanup scraper main function

--- scraper/index.js
+++ scraper/index.js
@@ -1,14 +1,83 @@
-const Scraper = require('./Scraper');
+const mongo = require('mongodb');
+
+const Scraper = require('./scraper');
+const MongoClient = require('./mongo');
+
+const Product = require('./models/product');
+const ProductUpdate = require('./models/product-update');
+const Roaster = require('./models/roaster');
+const RoasterUpdate = require('./models/roaster-update');
+
 const configs = require('./roasters.json');
 
-let scraper = Scraper();
-scraper.scrape(configs).then((data) => {
-    console.log(data);
-});
+const scrape = async () => {
+    let scraper = Scraper();
+    let scraped = await scraper.scrape(configs);
+
+    return scraped;
+}
+
+const findOrCreateRoaster = async (roasterData) => {
+    let roaster = await Roaster.findOne({fid: roasterData.fid});
+    if (!roaster) {
+        roaster = await Roaster.save({
+            name: roasterData.name,
+            fid: roasterData.fid
+        });
+    }
+
+    return roaster;
+};
+
+const addRoasterUpdate = async (roaster, updateTimestamp, rules) => {
+    await RoasterUpdate.save({
+        roasterId: roaster._id,
+        timestamp: updateTimestamp,
+        rules: rules,
+        status: RoasterUpdate.STATUS_SUCCESS
+    });
+};
+
+const findOrCreateProduct = async (productData, roasterId) => {
+    let product = await Product.findOne({fid: productData.link});
+    if (!product) {
+        product = await Product.save({
+            name: productData.name,
+            fid: productData.link,
+            roasterId: new mongo.ObjectID(roasterId),
+            image: productData.image,
+            link: productData.link
+        });
+    }
+
+    return product;
+};
+
+const addProductUpdate = async (productId, updateTimestamp, isOutOfStock) => {
+    let status = (isOutOfStock)
+        ? ProductUpdate.STATUS_UNAVAILABLE
+        : ProductUpdate.STATUS_AVAILABLE;
+
+    await ProductUpdate.save({
+        productId: productId,
+        timestamp: updateTimestamp,
+        status: status
+    });
+};
 
 (async () => {
-    let scraper = Scraper();
-    let data = await scraper.scrape(configs);
+    await MongoClient.connect();
 
-    console.log(data);
+    let scraped = await scrape();
+    let updateTimestamp = new Date().getTime();
+
+    for (let roasterData of scraped) {
+        let roaster = await findOrCreateRoaster(roasterData);
+        await addRoasterUpdate(roaster._id, updateTimestamp, roasterData.rules)
+
+        for (let productData of roasterData.products) {
+            let product = await findOrCreateProduct(productData, roaster._id);
+            await addProductUpdate(product._id, updateTimestamp, productData.isOutOfStock);
+        }
+    }
 })();
