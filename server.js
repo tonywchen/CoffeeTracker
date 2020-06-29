970392fbe5b25083a56852fed416d0fcc3463ce8
Tony Wei-Han Chen <tonywh.chen@gmail.com>
2020-06-29 15:22:07 -0400

    ensure mongo connects when server starts

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const MongoClient = require('./service/mongo');
+
 app.use(require('./routes'));
+
+MongoClient.connect();
 
 app.listen(port, () => console.log(`Example app listening at http://0.0.0.0:${port}`))
