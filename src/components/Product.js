cd4dbb1a3e219f69e22fb02ce90208215ce6e587
Tony Wei-Han Chen <tonywh.chen@gmail.com>
2020-07-11 15:43:32 -0400

    clean up product class

--- src/components/Product.js
+++ src/components/Product.js
@@ -14,57 +14,30 @@ class Product extends React.Component {
         });
     };
 
-    render() {
-        let {productId, productImage, productName, updates, createDate} = this.props.product;
-        let latestStatus;
-        let latestContainerClass;
-        let latestIconClass;
-
-        let datesToDisplay = _.takeRight(Object.keys(updates).sort(), 5);
-
-        let statuses = datesToDisplay.map(dateString => {
+    _computeStatuses = (updates, dateStrings, createDate) => {
+        let statuses = {};
+        for (let dateString of dateStrings) {
             let isAvailable = updates[dateString];
+            let status = Product.STATUS_NOT_APPLICABLE;
 
-            let currentStatus = {},
-                iconClass = '',
-                containerClass = '';
-
-            if (dateString < createDate) {
-                currentStatus.isNew = false;
-                currentStatus.available = Product.STATUS_NOT_APPLICABLE;
-            } else {
-                currentStatus.isNew = (dateString === createDate) && isAvailable;
-                currentStatus.available = (isAvailable)
+            if (dateString === createDate && isAvailable) {
+                status = Product.STATUS_NEW;
+            } else if (dateString > createDate) {
+                status = (isAvailable)
                     ? Product.STATUS_AVAILABLE
-                    : Product.STATUS_UNAVAILABLE
-                ;
+                    : Product.STATUS_UNAVAILABLE;
             }
 
-            switch (currentStatus.available) {
-                case Product.STATUS_NOT_APPLICABLE:
-                    containerClass = 'status not-applicable';
-                    iconClass = 'icon circle outline'
-                    break;
-                case Product.STATUS_AVAILABLE:
-                    if (currentStatus.isNew) {
-                        containerClass = 'status new';
-                        iconClass = 'icon fire';
-                    } else {
-                        containerClass = 'status available';
-                        iconClass = 'icon check';                        
-                    }
-                    break;
-                case Product.STATUS_UNAVAILABLE:
-                    containerClass = 'status unavailable';
-                    iconClass = 'icon ban';
-                    break;
-                default:
-                    break;
-            }
+            statuses[dateString] = status;
+        }
 
-            latestStatus = currentStatus;
-            latestContainerClass = containerClass;
-            latestIconClass = iconClass;
+        return statuses;
+    }
+
+    renderStatuses = (statuses, productId) => {
+        return Object.keys(statuses).sort().map((dateString) => {
+            let status = statuses[dateString];
+            let {iconClass, containerClass} = STATUS_TO_CLASSES[status];
 
             let date = moment(dateString, 'YYYY/MM/DD').format('MM/DD');
 
@@ -77,38 +50,39 @@ class Product extends React.Component {
                 </div>
             );
         });
-        
+    }
+
+    render() {
+        let {productId, productImage, productName, updates, createDate} = this.props.product;
+
+        let dateStrings = Object.keys(updates).sort();
+        let statuses = this._computeStatuses(updates, dateStrings, createDate);
+
+        let latestDateString = _.last(dateStrings);
+        let latestStatus = statuses[latestDateString];
+        let latestClasses = STATUS_TO_CLASSES[latestStatus];
+
         let collapsedClass = (this.state.collapsed)? 'product--collapsed' : '';
-        let availabilityClass = '';
-        switch (latestStatus.available) {
-            case Product.STATUS_AVAILABLE:
-                availabilityClass = 'product--available';
-                break;
-            case Product.STATUS_UNAVAILABLE:
-                availabilityClass = 'product--unavailable';
-                break;
-            default:
-                // do nothing
-        }
+        let availabilityClass = latestClasses.availabilityClass;
 
         let productClasses = ['product', collapsedClass, availabilityClass];
         let productClassName = productClasses.join(' ');
 
         return (
-            <div className="item" onClick={this.toggle}>
+            <div className="list-item" onClick={this.toggle}>
                 <div className={productClassName}>
                     <img src={productImage} className="product__image" alt={productName}></img>
                     <div className="product__content">
                         <div className="product__name">
                             <div className="product__name-inner">
                                 <span className="product__availability">
-                                    <i className={latestIconClass}></i>
+                                    <i className={latestClasses.iconClass}></i>
                                 </span>
                                 {productName}
                             </div>
                         </div>
                         <div className="statuses">
-                            {statuses}
+                            {this.renderStatuses(statuses, productId)}
                         </div>
                     </div>
                 </div>
@@ -117,8 +91,32 @@ class Product extends React.Component {
     }
 }
 
+Product.STATUS_NEW = 2;
 Product.STATUS_AVAILABLE = 1;
 Product.STATUS_NOT_APPLICABLE = 0;
 Product.STATUS_UNAVAILABLE = -1;
+
+const STATUS_TO_CLASSES = {};
+STATUS_TO_CLASSES[Product.STATUS_NEW] = {
+    containerClass: 'status new',
+    iconClass: 'icon fire',
+    availabilityClass: 'product--available'
+};
+STATUS_TO_CLASSES[Product.STATUS_AVAILABLE] = {
+    containerClass: 'status available',
+    iconClass: 'icon check',
+    availabilityClass: 'product--available'
+};
+STATUS_TO_CLASSES[Product.STATUS_NOT_APPLICABLE] = {
+    containerClass: 'status not-applicable',
+    iconClass: 'icon circle outline',
+    availabilityClass: ''
+};
+STATUS_TO_CLASSES[Product.STATUS_UNAVAILABLE] = {
+    containerClass: 'status unavailable',
+    iconClass: 'icon ban',
+    availabilityClass: 'product--unavailable'
+};
+
 
 export default Product;
