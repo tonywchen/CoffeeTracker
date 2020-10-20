7f28f5dfc5ef44319988e1df7e7e644c3649850f
Tony Wei-Han Chen <tonywh.chen@gmail.com>
2020-10-20 11:00:22 -0400

    add tooltip for status

--- client/src/components/products/Product.js
+++ client/src/components/products/Product.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactTooltip from 'react-tooltip';
 
 import _ from 'lodash';
 import moment from 'moment';
@@ -43,17 +44,23 @@ class Product extends React.Component {
     renderStatuses = (statuses, productId) => {
         return Object.keys(statuses).sort().map((dateString) => {
             let status = statuses[dateString];
-            let {iconClass, containerClass} = STATUS_TO_CLASSES[status];
+            let {iconClass, containerClass, tooltip} = STATUS_DATA[status];
 
             let date = moment(dateString, DATE_FORMAT).format(DATE_FORMAT_SHORT);
 
             return (
-                <div className={containerClass} key={`${productId}-${date}`}>
-                    <div className="status__inner">
-                        <i className={iconClass} />
-                        <div>{date}</div>
+                <>
+                    <div
+                        className={containerClass} 
+                        key={`${productId}-${date}`} 
+                        data-tip={tooltip}>
+                        <div className="status__inner">
+                            <i className={iconClass} />
+                            <div>{date}</div>
+                        </div>
                     </div>
-                </div>
+                    <ReactTooltip delayShow={100} border borderColor="rgba(255, 255, 255, 0.15)"></ReactTooltip>
+                </>
             );
         });
     }
@@ -64,7 +71,7 @@ class Product extends React.Component {
         let dateStrings = Object.keys(statuses).sort();
         let latestDateString = _.last(dateStrings);
         let latestStatus = statuses[latestDateString];
-        let latestClasses = STATUS_TO_CLASSES[latestStatus];
+        let latestClasses = STATUS_DATA[latestStatus];
 
         let collapsedClass = (this.state.collapsed)? 'product--collapsed' : '';
         let availabilityClass = latestClasses.availabilityClass;
@@ -105,33 +112,38 @@ Product.STATUS_NOT_APPLICABLE = 0;
 Product.STATUS_UNAVAILABLE = -1;
 Product.STATUS_UNKNOWN = -2;
 
-const STATUS_TO_CLASSES = {};
-STATUS_TO_CLASSES[Product.STATUS_NEW] = {
+const STATUS_DATA = {};
+STATUS_DATA[Product.STATUS_NEW] = {
     containerClass: 'status new',
     iconClass: 'icon fire',
-    availabilityClass: 'product--available'
+    availabilityClass: 'product--available',
+    tooltip: 'This bean is newly released!'
 };
-STATUS_TO_CLASSES[Product.STATUS_AVAILABLE] = {
+STATUS_DATA[Product.STATUS_AVAILABLE] = {
     containerClass: 'status available',
     iconClass: 'icon check',
-    availabilityClass: 'product--available'
+    availabilityClass: 'product--available',
+    tooltip: 'This bean is in stock'
 };
-STATUS_TO_CLASSES[Product.STATUS_PARTIALLY_AVAILABLE] = {
+STATUS_DATA[Product.STATUS_PARTIALLY_AVAILABLE] = {
     containerClass: 'status available',
     iconClass: 'icon exclamation',
-    availabilityClass: 'product--available'
+    availabilityClass: 'product--available',
+    tooltip: 'This availability of this bean has fluctuated throughout the day'
 };
-STATUS_TO_CLASSES[Product.STATUS_NOT_APPLICABLE] = {
+STATUS_DATA[Product.STATUS_NOT_APPLICABLE] = {
     containerClass: 'status not-applicable',
     iconClass: 'icon circle outline',
-    availabilityClass: ''
+    availabilityClass: '',
+    tooltip: 'No stock info is available for this bean'
 };
-STATUS_TO_CLASSES[Product.STATUS_UNAVAILABLE] = {
+STATUS_DATA[Product.STATUS_UNAVAILABLE] = {
     containerClass: 'status unavailable',
     iconClass: 'icon ban',
-    availabilityClass: 'product--unavailable'
+    availabilityClass: 'product--unavailable',
+    tooltip: 'This bean is not in stock'
 };
-STATUS_TO_CLASSES[Product.STATUS_UNKNOWN] = {
+STATUS_DATA[Product.STATUS_UNKNOWN] = {
     containerClass: 'status unknown',
     iconClass: 'icon question',
     availabilityClass: 'product--unknown'
